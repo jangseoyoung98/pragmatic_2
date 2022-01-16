5129efcaf1ec676d12eeb93e5f6893c072f8941f
jangseoyoung <jangseoyoung98@gmail.com>
2022-01-16 22:17:40 +0900

    django course 14 commit

--- pragmatic/accountApp/models.py
+++ pragmatic/accountApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# $ python manage.py makemigrations
+# 모델스 파이에다 쓰는 내용을 DB와 연동시킬 파이썬 파일로 만들어주는 작업
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False)
