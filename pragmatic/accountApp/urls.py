13a0cd7ae8d6ad77bad92c1aa04361579d1362db
jangseoyoung <jangseoyoung98@gmail.com>
2022-01-21 17:11:34 +0900

    django course 22 commit

--- pragmatic/accountApp/urls.py
+++ pragmatic/accountApp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountApp.views import hello_world, AccountCreateView
@@ -6,6 +7,9 @@ app_name = "accountApp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountApp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
