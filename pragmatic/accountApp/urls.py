b3e328842a257032c42bd2b5b33684f60c047b50
jangseoyoung <jangseoyoung98@gmail.com>
2022-01-21 16:28:57 +0900

    django course 21 commit

--- pragmatic/accountApp/urls.py
+++ pragmatic/accountApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountApp.views import hello_world
+from accountApp.views import hello_world, AccountCreateView
 
 app_name = "accountApp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- pragmatic/accountApp/views.py
+++ pragmatic/accountApp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 from accountApp.models import HelloWorld
+from django.views.generic import CreateView
 
 
 def hello_world(request):
@@ -21,3 +24,14 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView): # class 베이스 뷰가 간단하고 직관적이다!
+    model = User  # 장고 기본 제공 모델
+    form_class = UserCreationForm # 장고 기본 제공 폼
+    success_url = reverse_lazy('accountApp:hello_world')  # 계정 만들기 성공 했으면, 어느 경로로 연결할 것인가?
+    template_name = 'accountApp/create.html'  # 어느 html 파일을 통해서 볼지?
+
+
+
+
