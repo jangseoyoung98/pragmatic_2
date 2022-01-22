82a8e593bf38ca1a5c7c957694fd53987e1776c8
jangseoyoung <jangseoyoung98@gmail.com>
2022-01-22 16:49:09 +0900

    django course 27 commit

--- pragmatic/accountApp/views.py
+++ pragmatic/accountApp/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 
@@ -12,19 +12,23 @@ from accountApp.forms import AccountUpdateForm
 
 
 def hello_world(request):
+    
+    # 로그인 되어 있을 경우에만 헬로월드에 접속할 수 있도록 하는 것
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
 
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        hello_world_list = HelloWorld.objects.all()
-        return HttpResponseRedirect(reverse('accountApp:hello_world'))
+            hello_world_list = HelloWorld.objects.all()
+            return HttpResponseRedirect(reverse('accountApp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountApp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountApp:login'))
 
 
 class AccountCreateView(CreateView): # class 베이스 뷰가 간단하고 직관적이다!
@@ -41,12 +45,37 @@ class AccountDetailView(DetailView):
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
-    form_class = AccountUpdateForm # Form을 바꿔줌
+    form_class = AccountUpdateForm
     success_url = reverse_lazy('accountApp:hello_world')
     template_name = 'accountApp/update.html'
+
+    # 업데이트의 경우도 로그인 되어 있을 경우에만 가능하도록 + 타 계정에는 접속할 수 없도록!
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
 
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountApp:login')
     template_name = 'accountApp/delete.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().post(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
--- pragmatic/pragmatic/urls.py
+++ pragmatic/pragmatic/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accountsg/', include('accountApp.urls')),
+    path('accounts/', include('accountApp.urls')),
 
 ]
